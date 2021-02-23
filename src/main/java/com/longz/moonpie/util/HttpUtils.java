384ec8deeffcfeef2c53d57816eaa0f1c56174c8
陈鸣明 <leo@loongz.com>
2021-02-23 17:37:02 +0800

    初始化提交

---
+++ src/main/java/com/longz/moonpie/util/HttpUtils.java
@@ -0,0 +1,78 @@
+package com.longz.moonpie.util;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+/**
+ * 通用http发送方法
+ *
+ * @author Jason
+ */
+public class HttpUtils {
+
+
+    public static String sendHttpPost(String url, String JSONBody, String authorization) throws Exception {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.addHeader("Content-Type", "application/json");
+        httpPost.addHeader("Authorization", authorization);
+        httpPost.setEntity(new StringEntity(JSONBody));
+        CloseableHttpResponse response = httpClient.execute(httpPost);
+        HttpEntity entity = response.getEntity();
+        String responseContent = EntityUtils.toString(entity, "UTF-8");
+        response.close();
+        httpClient.close();
+        return responseContent;
+    }
+
+    public static String sendHttpPostSign(String url, String JSONBody, String sign) throws Exception {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.addHeader("Content-Type", "application/json;charset=utf-8");
+        httpPost.addHeader("Sign", sign);
+        httpPost.setEntity(new StringEntity(JSONBody));
+        CloseableHttpResponse response = httpClient.execute(httpPost);
+        HttpEntity entity = response.getEntity();
+        String responseContent = EntityUtils.toString(entity, "UTF-8");
+        response.close();
+        httpClient.close();
+        return responseContent;
+    }
+
+    public static String sendHttpPost(String url, String JSONBody) throws Exception {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.addHeader("Content-Type", "application/json;charset=utf-8");
+        httpPost.setEntity(new StringEntity(JSONBody));
+        CloseableHttpResponse response = httpClient.execute(httpPost);
+        HttpEntity entity = response.getEntity();
+        String responseContent = EntityUtils.toString(entity, "UTF-8");
+        response.close();
+        httpClient.close();
+        return responseContent;
+    }
+
+    public static String sendHttpGet(String url, String authorization) throws Exception {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        HttpGet httpGet = new HttpGet(url);
+        httpGet.addHeader("Content-Type", "application/json");
+        httpGet.addHeader("Authorization", authorization);
+        CloseableHttpResponse response = httpClient.execute(httpGet);
+        HttpEntity entity = response.getEntity();
+        String responseContent = EntityUtils.toString(entity, "UTF-8");
+        response.close();
+        httpClient.close();
+        return responseContent;
+    }
+
+}
